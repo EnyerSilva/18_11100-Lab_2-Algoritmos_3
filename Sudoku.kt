5b842b98884288ae8a87c8aae786b6685ba80a8e
Enyerber Silva <18-11100@usb.ve>
2026-03-06 13:44:35 -0400

    Update Sudoku.kt

--- Sudoku.kt
+++ Sudoku.kt
@@ -6,18 +6,27 @@
 */
 
 fun main(args: Array<String>) {
+    /*
+     * Si el usario no ingresa exactamente 81 dígitos o si no ingresa nada, entonces se le imprime en pantalla
+     * el requerimiento para iniciar el solver de Sudoku
+     */
     if (args.isEmpty() || args[0].length != 81){
         println ("Ingrese una cadena de caracteres de 81 dígitos para iniciar el solver de Sudoku")
         return
     }
 
-    val cadenaEntrada = args[0]
-    val tablero = Array(9) {IntArray(9)}
+    val cadenaEntrada = args[0] // Cadena de entrada del usuario
+    val tablero = Array(9) {IntArray(9)} // Matriz 9x9 del tablero
 
     for (i in 0 until 81) {
-        tablero[i/9][i%9] = cadenaEntrada[i] - '0'
+        tablero[i/9][i%9] = cadenaEntrada[i] - '0' // Se resta 0 para que lo lea como número y no como carácter
+                                                   // Convierte cada char en int (Por su código ASCII)
     }
 
+    /*
+     *En este punto hacemos una poda preventiva en el tablero inicial
+     *Así verificamos si el tablero inicial es inválido y no perdemos tiempo
+     */
     for (f in 0 until 9) {
         for (c in 0 until 9) {
             val num = tablero[f][c]
@@ -32,10 +41,10 @@ fun main(args: Array<String>) {
         }
     }
 
+    // Llamamos a la función resolver e iniciamos la solución desde la casilla (0,0)
     if(resolver(tablero, 0, 0)) {
         imprimirTablero(tablero)
     }
-
     else {
         println("NOSOLUTION")
     }
