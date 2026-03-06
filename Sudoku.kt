8dfdd922613b23e120b42f05437b580ce92539cb
Enyerber Silva <18-11100@usb.ve>
2026-03-06 12:52:59 -0400

    Update Sudoku.kt

--- Sudoku.kt
+++ Sudoku.kt
@@ -1,6 +1,6 @@
 /*
 * Laboratorio 2: Solver Sudoku
-* Implementaciómn usando ténica de Backtraking
+* Implementación usando ténica de Backtraking
 * Estudiante: Enyerber Silva
 * Carnet: 18-11100
 */
